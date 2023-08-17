4ba35e8399a495769075687e8216d38b42d35cd5
Victor Nova <victor@borg.games>
2023-08-16 17:03:35 -0700

    play Factorio demo without login

---
+++ js/ephemeral.js
@@ -0,0 +1,83 @@
+﻿import { wait } from "./streaming-client/src/util.js";
+
+export class Ephemeral {
+    async connect(cfg, sessionId, answer, onCandidate) {
+        this.onCandidate = onCandidate;
+        this.sessionId = sessionId;
+        this.answer = answer;
+
+        await this.submitAnswer();
+
+        console.debug('answer submitted');
+
+        this.fetchCandidates().then(() => console.debug('candidates fetch stopped'));
+    }
+
+    async submitAnswer() {
+        const response = await fetch(this.endpoint + this.sessionId + '/answer', {
+            method: 'PUT',
+            headers: {'Content-Type': 'text/plain'},
+            body: JSON.stringify(this.answer)
+        });
+
+        if (!response.ok)
+            throw new Error('Failed to submit answer');
+    }
+
+    async sendCandidate(candidate) {
+        const response = await fetch(this.endpoint + this.sessionId + '/answerIce', {
+            method: 'PUT',
+            headers: {'Content-Type': 'text/plain'},
+            body: candidate
+        });
+
+        if (!response.ok)
+            throw new Error('Failed to submit ICE candidate');
+    }
+    
+    static async getNodes() {
+        const response = await fetch('https://queen-prototype.azurewebsites.net/ephemeral/offers');
+        if (!response.ok)
+            throw new Error('Failed to fetch nodes');
+
+        return await response.json();
+    }
+
+    async fetchCandidates() {
+        const seen = new Set();
+        while (this.stopCode === 0) {
+            const response = await fetch(this.endpoint + this.sessionId);
+            if (!response.ok)
+                throw new Error('Failed to fetch ICE candidates');
+
+            const session = await response.json();
+            for (const candidate of session.offerIce) {
+                if (seen.has(candidate))
+                    continue;
+                await this.onCandidate(candidate, null);
+                seen.add(candidate);
+            }
+
+            await wait(1000);
+        }
+    }
+
+    cfgDefaults(cfg) {
+        if (!cfg) cfg = {};
+        return structuredClone(cfg);
+    }
+
+    getAttemptId() {
+        console.warn('getAttemptId stub');
+        return 41;
+    }
+
+    constructor(endpoint) {
+        this.endpoint = endpoint || 'https://queen-prototype.azurewebsites.net/ephemeral/';
+        this.stopCode = 0;
+    }
+
+    close(code) {
+        this.stopCode = code;
+    }
+}
---
+++ js/home.js
@@ -0,0 +1,123 @@
+﻿import {Client} from './streaming-client/src/client.js';
+import {ClientAPI} from "./client-api.js";
+import {Ephemeral} from "./ephemeral.js";
+import {Session} from "./session.js";
+
+import * as util from "./streaming-client/src/util.js";
+import * as Msg from './streaming-client/src/msg.js';
+import {getNetworkStatistics} from "./connectivity-check.js";
+
+
+const clientApi = new ClientAPI();
+const status = document.getElementById('game-status');
+const video = document.getElementById('stream');
+
+export class Home {
+    static runClient(nodes, timeout) {
+        const signalFactory = (onFatal) => new Ephemeral();
+
+        return new Promise(async (resolve) => {
+            const clients = [];
+            function killOthers(current){
+                console.log('we have a winner!');
+                for(let j = 0; j < clients.length; j++){
+                    if (clients[j] !== current)
+                        clients[j].destroy(Client.StopCodes.CONCURRENT_SESSION);
+                }
+                clients.length = 1;
+                clients[0] = current;
+            }
+            for(let i = 0; i < nodes.length; i++) {
+                const offer = nodes[i];
+                //set up client object with an event callback: gets connect, status, chat, and shutter events
+                const client = new Client(clientApi, signalFactory, video, (event) => {
+                    console.log('EVENT', event);
+
+                    switch (event.type) {
+                        case 'exit':
+                            document.removeEventListener('keydown', hotkeys, true);
+                            if (event.code !== Client.StopCodes.CONCURRENT_SESSION)
+                                resolve(event.code);
+                            else
+                                clients.removeByValue(client);
+                            break;
+                        case 'status':
+                            status.innerText = event.msg;
+                            break;
+                    }
+                }, async (name, channel) => {
+                    switch (name) {
+                        case 'control':
+                            await Session.waitForCommandRequest(channel);
+                            const stats = await getNetworkStatistics(channel);
+                            await Session.waitForCommandRequest(channel);
+                            killOthers(client);
+                            channel.send(Msg.launch());
+                            await Session.waitForCommandRequest(channel);
+                            break;
+                    }
+                });
+                clients.push(client);
+                
+                //set up useful hotkeys that call client methods: destroy can also be used to cancel pending connection
+                const hotkeys = (event) => {
+                    event.preventDefault();
+
+                    if (event.code === 'Backquote' && event.ctrlKey && event.altKey) {
+                        client.destroy(0);
+                    } else if (event.code === 'Enter' && event.ctrlKey && event.altKey) {
+                        util.toggleFullscreen(client.element);
+                    }
+                };
+                document.addEventListener('keydown', hotkeys, true);
+
+                async function run(){
+                    try {
+                        const info = JSON.parse(offer.peer_connection_offer);
+                        const sdp = JSON.parse(info.Offer);
+
+                        const encoder_bitrate = parseInt(localStorage.getItem('encoder_bitrate')) || 1;
+
+                        await Promise.race([
+                            timeout,
+                            client.connect(offer.session_id, sdp, {
+                                encoder_bitrate
+                            })]);
+                    } catch (e) {
+                        if (clients.removeByValue(client) && clients.length === 0)
+                            resolve(e);
+                    }
+                }
+                run();
+            }
+        });
+    }
+
+    static async launch(sessionId) {
+        document.body.classList.add('video');
+
+        const timeout = util.timeout(1000 /*s*/ * 60 /*m*/ * 3);
+
+        try {
+            if (!sessionId)
+                sessionId = crypto.randomUUID();
+            status.innerText = 'looking for a node...';
+            const nodes = await Ephemeral.getNodes();
+            if (nodes.length === 0)
+                throw new Error('No nodes currently available. Try again later.');
+
+            const code = await Home.runClient(nodes, timeout);
+
+            if (code !== 0)
+                alert(`Exit code: ${code}`);
+        } catch (e) {
+            console.error(e);
+            alert(e);
+        } finally {
+            document.body.classList.remove('video');
+
+            video.src = '';
+            video.load();
+        }
+    }
+}
