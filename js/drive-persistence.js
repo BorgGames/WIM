0fe3e31c086ba7814e638f95331d2952467ec1dd
Victor Nova <victor@borg.games>
2023-08-17 15:25:17 -0700

    a simple forwarder for OneDrive calls

---
+++ js/drive-persistence.js
@@ -0,0 +1,34 @@
+﻿import {makeRequest} from "./onedrive.js";
+
+const prefix = "special/approot:";
+
+async function forwarder(event) {
+    const request = JSON.parse(event.data);
+    try {
+        if (!request.uri.startsWith('app:'))
+            throw new Error('Invalid URI');
+        const url = prefix + request.uri.substring('app:'.length);
+        const response = await makeRequest(url, request.options);
+        const text = await response.text();
+        event.target.send(JSON.stringify({
+            status: response.status,
+            text
+        }));
+    } catch (e) {
+        event.target.send(JSON.stringify({
+            status: 502,
+            text: JSON.stringify(e)
+        }));
+    }
+}
+
+export class OneDrivePersistence {
+    constructor(channel) {
+        this.channel = channel;
+        channel.addEventListener('message', forwarder);
+    }
+
+    destroy() {
+        this.channel.removeEventListener('message', forwarder);
+    }
+}
