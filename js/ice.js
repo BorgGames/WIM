ff39d9d92283ca1b295aaa630b41b026ca061990
Victor Nova <victor@borg.games>
2023-09-06 11:23:52 -0700

    added free tier TURN

---
+++ js/ice.js
@@ -0,0 +1,15 @@
+﻿export function getIceServers() {
+    let json = localStorage.getItem('iceServers');
+    if (json === null)
+        return [];
+    return JSON.parse(json);
+}
+
+export async function updateIceServers() {
+    let response = await fetch("https://borg.metered.live/api/v1/turn/credentials?apiKey=" + API_KEY);
+    if (!response.ok)
+        return;
+    localStorage.setItem('iceServers', await response.text());
+}
+
+const API_KEY = "c8f6f80012a6fff80645fc4194580912564a";
